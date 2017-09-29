b539df9aa31762d8163331fd1a496d9c83a18e94
Matthew Newville <newville@cars.uchicago.edu>
2017-09-28 21:03:48 -0500

    xraylarch recipe should install fabio and pyFAI from pip

--- xraylarch/bld.bat
+++ xraylarch/bld.bat
@@ -1,2 +1,4 @@
+
+"%SCRIPTS%\pip.exe" install --upgrade fabio pyFAI
 "%PYTHON%" setup.py install
 if errorlevel 1 exit 1
--- xraylarch/build.sh
+++ xraylarch/build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
+$PREFIX/bin/pip install --upgrade fabio pyFAI
 $PYTHON setup.py install
-
