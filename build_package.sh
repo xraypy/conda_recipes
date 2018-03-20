6684fe313c4ebd4d5fd4a9e8693924af5b996153
Matthew Newville <newville@cars.uchicago.edu>
2018-03-20 11:25:06 -0500

    fix build script to better determine conda root

--- build_package.sh
+++ build_package.sh
@@ -4,13 +4,17 @@ PACKAGE=$1
 
 PLAT=`conda info -a | grep platform | sed 's/[ ]*platform :[ ]*//g'`
 
-ROOT=`conda info -a | grep 'default environment' | sed 's/[ ]*default environment :[ ]*//g'`
+ROOT=`conda info -a | grep 'sys.prefix:' | sed 's/[ ]*sys.prefix:[ ]*//g'`
 
 echo 'Building Package ' $PACKAGE
 
 conda build $PACKAGE
 
+cp $ROOT/conda-bld/$PLAT/$PACKAGE* dist/$PLAT/.
+
+
 conda convert --platform all $ROOT/conda-bld/$PLAT/$PACKAGE* -o dist
+
 
 echo "# Now do"
 echo '# '    anaconda upload -u gsecars dist/\*/$PACKAGE\*
