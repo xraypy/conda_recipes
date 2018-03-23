ec8a506df5064c5943a3222110cf751b35e09590
Matthew Newville <newville@cars.uchicago.edu>
2018-03-23 17:12:12 -0500

    do not install fabio, pyfai from pip

--- xraylarch/bld.bat
+++ xraylarch/bld.bat
@@ -1,4 +1,3 @@
 
-"%SCRIPTS%\pip.exe" install --upgrade --use-wheel fabio pyFAI
 "%PYTHON%" setup.py install
 if errorlevel 1 exit 1
--- xraylarch/build.sh
+++ xraylarch/build.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
 $PYTHON setup.py install
-
-$PREFIX/bin/pip install --upgrade --use-wheel fabio pyFAI
