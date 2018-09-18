5dac4f30ad5d3c5dfe5c3783407609856ce4beb9
Matthew Newville <newville@cars.uchicago.edu>
2018-09-18 11:17:18 -0500

    update tomopy recipe to use setup_nocompile.py on Windows and MacOS

--- tomopy/build.sh
+++ tomopy/build.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
-export C_INCLUDE_PATH=$PREFIX/include
-$PYTHON setup.py install
 
+osname=`uname -s`
+
+echo "IN build.sh :: "  $osname
+
+# for Darwin, we do no try to compile extension
+if [[ "$osname" == 'Darwin' ]] ; then
+    $PYTHON setup_nocompile.py install
+
+elif [[ "$osname" == 'Linux' ]] ; then
+    export C_INCLUDE_PATH=$PREFIX/include
+    $PYTHON setup.py install
+fi
